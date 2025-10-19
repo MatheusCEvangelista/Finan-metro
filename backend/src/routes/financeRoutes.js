7e3821b95c93641955cc6e1a2c3668bdf57563af
MatheusCEvangelista <matcastroea@gmail.com>
2025-10-18 21:01:41 -0300

    teste

--- backend/src/routes/financeRoutes.js
+++ backend/src/routes/financeRoutes.js
@@ -6,8 +6,7 @@ const router = express.Router();
 
 router.post("/", protect, createFinance);
 router.get("/", protect, getFinances);
-router.put("/: id", protect, updateFinance);
-router.delete("/: id", protect, deleteFinance);
+
 
 export default router;
 
